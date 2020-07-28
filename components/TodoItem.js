f6fb695dbe24cb87d9189ac8c61fce97b0ecb6b2
kukori <attila.varjas@gmail.com>
2020-07-28 16:28:32 +0700

    Icons added

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const TodoItem = ({item, onItemPress}) => {
     return (
-        <TouchableOpacity onPress={() => onItemPress(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity>
+            <View style={styles.item}>
+                <MaterialIcons name="delete" size={18} color="indianred" onPress={() => onItemPress(item.key)}/>
+                <Text style={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
         borderWidth: 1,
         borderRadius: 10
+    },
+    itemText: {
+        marginLeft: 10
     }
 });
 
